7ab9ae16260e34afd7887bff9e0d8c68cd5ada66
Ayo <Ayoroq@gmail.com>
2025-12-13 15:52:14 -0500

    added css stylings to the form

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,10 +1,29 @@
-export default function New(){
-    return (
-        <main>
-            <h2>New</h2>
-            <form action="">
-                
-            </form>
-        </main>
-    )
+import styles from "../styles.module.css";
+export default function New() {
+  return (
+    <main>
+      <p className={styles.title}>New Notice</p>
+      <form action="" className={styles.form}>
+        <p>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" required />
+        </p>
+        <p>
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" required />
+        </p>
+        <p>
+          <label htmlFor="message">Message</label>
+          <textarea
+            name="message"
+            id="message"
+            required
+                cols={30}
+                rows={10}
+          ></textarea>
+        </p>
+        <button type="submit" className={styles.submit}>Submit</button>
+      </form>
+    </main>
+  );
 }
