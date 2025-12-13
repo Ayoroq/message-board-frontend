5344f1242d94929223bf4e7c0b5e9bdc849343d9
Ayo <Ayoroq@gmail.com>
2025-12-13 16:09:25 -0500

    Handled the input values to use a react method

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,28 +1,56 @@
 import styles from "../styles.module.css";
+import { useState } from "react";
 export default function New() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    
+  }
+
   return (
     <main>
       <p className={styles.title}>New Notice</p>
-      <form action="" className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <p>
           <label htmlFor="username">Username</label>
-          <input type="text" name="username" id="username" required />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </p>
         <p>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" required />
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </p>
         <p>
           <label htmlFor="message">Message</label>
           <textarea
             name="message"
             id="message"
+            value={text}
             required
-                cols={30}
-                rows={10}
+            cols={30}
+            rows={10}
+            onChange={(e) => setText(e.target.value)}
           ></textarea>
         </p>
-        <button type="submit" className={styles.submit}>Submit</button>
+        <button type="submit" className={styles.submit}>
+          Submit
+        </button>
       </form>
     </main>
   );
