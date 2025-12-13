7f1fb865aaf25217f222545e145849f978c97bac
Ayo <Ayoroq@gmail.com>
2025-12-13 10:12:59 -0500

    Added navbar and footer and basic information to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import gsap from 'gsap'
-import { ReactLenis } from 'lenis/react'
-import { useEffect, useRef } from 'react'
-import './App.css'
+import gsap from "gsap";
+import { ReactLenis } from "lenis/react";
+import { useEffect, useRef } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
-  const lenisRef = useRef()
-  
+  const lenisRef = useRef();
+
   useEffect(() => {
     function update(time) {
-      lenisRef.current?.lenis?.raf(time * 1000)
+      lenisRef.current?.lenis?.raf(time * 1000);
     }
-  
-    gsap.ticker.add(update)
-  
-    return () => gsap.ticker.remove(update)
-  }, [])
-  
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
-    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
-  )
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef}>
+      <Navbar />
+      <main>
+        <h1>Scroll down</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos aperiam
+          sint tempora totam doloribus maiores in voluptas, at error
+          reprehenderit dolor alias quaerat vero! Delectus inventore iste
+          quisquam labore odit.
+        </p>
+      </main>
+      <Footer />
+    </ReactLenis>
+  );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import style from './Components.module.css'
+
+export default function Footer(){
+    return(
+        <nav className={style.nav}>
+            
+        </nav>
+    )
+}
