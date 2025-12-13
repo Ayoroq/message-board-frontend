4f7e348ca66f33aad7f25c5500315f5acf7aa381
Ayo <Ayoroq@gmail.com>
2025-12-13 12:37:31 -0500

    finished the set-up that allows the backend and frontend to communicate

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,25 @@
-import styles from '../styles.module.css'
+import styles from "../styles.module.css";
+import { useState, useEffect } from "react";
 export default function Home() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:4000/");
+      const responseData = await response.json();
+      if (response.ok) {
+        setData(responseData);
+      } else {
+        console.error("Error fetching data:", responseData.error);
+      }
+    }
+    fetchData().catch(console.error);
+  }, []);
+
   return (
     <div className={styles.home}>
-      <h1>Home</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-        voluptatum.
-      </p>
+      <main>
+        {data && data.map((message) => <p key={message.id}>{message.text}</p>)}
+      </main>
     </div>
   );
 }
