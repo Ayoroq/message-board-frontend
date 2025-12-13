30bd5c3cb005dbef1cbd51700d8180885d7cbaf8
Ayo <Ayoroq@gmail.com>
2025-12-13 10:03:01 -0500

    Modifying the app with the addition of lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import gsap from 'gsap'
+import { ReactLenis } from 'lenis/react'
+import { useEffect, useRef } from 'react'
 import './App.css'
 
-function App() {
-
+export default function App() {
+  const lenisRef = useRef()
+  
+  useEffect(() => {
+    function update(time) {
+      lenisRef.current?.lenis?.raf(time * 1000)
+    }
+  
+    gsap.ticker.add(update)
+  
+    return () => gsap.ticker.remove(update)
+  }, [])
+  
   return (
-    <>
-    </>
+    <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
   )
 }
-
-export default App
