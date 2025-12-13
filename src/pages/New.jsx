e6530c8f5a143ca7f549a9a373bf48739ca8e21f
Ayo <Ayoroq@gmail.com>
2025-12-13 16:15:11 -0500

    Added the post data function to send data to the backend

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,13 +1,34 @@
 import styles from "../styles.module.css";
 import { useState } from "react";
+import { useNavigate } from "react-router";
+
 export default function New() {
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [text, setText] = useState("");
 
   function handleSubmit(e) {
     e.preventDefault();
-    
+    async function postData() {
+      const response = await fetch("http://localhost:4000/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, email, text }),
+      });
+      const responseData = await response.json();
+      if (response.ok) {
+        setUsername("");
+        setEmail("");
+        setText("");
+        navigate("/");
+      } else {
+        console.error("Error posting data:", responseData.error);
+      }
+    }
+    postData().catch(console.error);
   }
 
   return (
